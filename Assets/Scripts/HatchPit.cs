e351ef969659b54e7c6538e8ee77b4b54890fb9c
Tom Kattouw <tomkattouw@hotmail.com>
2021-01-30 00:36:30 +0100

    Smol fix

--- Assets/Scripts/HatchPit.cs
+++ Assets/Scripts/HatchPit.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 
 public class HatchPit : MonoBehaviour
 {
-	private void OnCollisionEnter(Collision collision)
-	{
-		// TODO dont destroy but use Justins interface
-		Destroy(collision.gameObject);
-	}
-
 	private void OnTriggerEnter(Collider other)
 	{
 		// TODO dont destroy but use Justins interface
