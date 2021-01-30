ecb4f9815fe27dad7cf7122278da1ec758875192
Tom Kattouw <tomkattouw@hotmail.com>
2021-01-30 12:25:47 +0100

    Menu + highscores

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	public void SwitchScene(int nextScene)
+	{
+		SceneManager.LoadScene(nextScene);
+	}
+
+	public void Exit()
+	{
+		Application.Quit();
+	}
+}
