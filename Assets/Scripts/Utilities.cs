bfefcca50679fadea45a323ccac249426f715189
Willem Meijer <willem1998m@hotmail.com>
2021-01-30 22:08:40 +0100

    Score fix

--- Assets/Scripts/Utilities.cs
+++ Assets/Scripts/Utilities.cs
@@ -4,11 +4,10 @@ public static class Utilities
 {
 	public static int SafeEvaluate(this AnimationCurve curve,  int alpha)
 	{
-		if (alpha > curve.length)
+		if (alpha > curve.keys[curve.length - 1].time)
 		{
 			return (int) curve.Evaluate(curve.length);
 		}
 		return (int) curve.Evaluate(alpha);
 	}
-
 }
