702ff21be2f57e8af60fc995ab4f7d351aa33e2c
Tom Kattouw <tomkattouw@hotmail.com>
2021-01-30 13:10:15 +0100

    lopende conveyor band

---
+++ Assets/Scripts/ConveyorBelt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+	Renderer renderer;
+
+	[SerializeField] float animSpeed = 0.5f;
+
+	float t = 0;
+
+    void Start()
+    {
+		renderer = GetComponent<Renderer>();
+	}
+
+    void Update()
+    {
+		t -= Time.deltaTime * animSpeed;
+		renderer.material.SetTextureOffset("_MainTex", new Vector2(0, t));
+	}
+}
