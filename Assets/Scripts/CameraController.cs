7d0f167734cc9b7d3ce05fe2561c8ed43e598a20
Tom Kattouw <tomkattouw@hotmail.com>
2021-01-30 17:04:12 +0100

    camera script

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	[SerializeField] Transform _player;
+	[SerializeField] Transform _sceneCenter;
+	[SerializeField] float _strength;
+	[SerializeField] float _zOffset;
+
+	Vector3 _playerPos = new Vector3();
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+		_playerPos = new Vector3(
+			_player.position.x,
+			_sceneCenter.position.y,
+			_player.position.z) - _zOffset * Vector3.forward;
+
+		transform.position =
+			Vector3.Lerp(_sceneCenter.position, _playerPos, _strength);
+    }
+}
