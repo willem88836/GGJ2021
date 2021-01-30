2f45157145260fb427e40a6b1bc050066cb29aea
Tom Kattouw <tomkattouw@hotmail.com>
2021-01-30 22:34:45 +0100

    particle

---
+++ Assets/Scripts/BlowerParticle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlowerParticle : MonoBehaviour
+{
+	ParticleSystem _particle;
+
+    void Start()
+    {
+		_particle = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Space))
+			ToggleParticle();
+    }
+
+	void ToggleParticle()
+	{
+		if (_particle.isPlaying)
+			_particle.Stop();
+		else
+			_particle.Play();
+	}
+}
