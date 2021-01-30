fa2de1580569cf86a28417addf8f4a57f3609ce2
Justin Geschiere <j.geschiere@hotmail.com>
2021-01-30 11:33:36 +0100

    Objectspawner integration with truck

---
+++ Assets/Scripts/Object_Spawning/ObjectSpawner.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private List<SpawnPoolEntry> spawnPools;
+
+	[Space]
+	[SerializeField]
+	private float spawnDelay;
+	[SerializeField]
+	private float spawnCount;
+	[SerializeField]
+	private Vector2 spawnForce;
+	[SerializeField]
+	private float directionFactor;
+
+	[Space]
+	[SerializeField]
+	private bool debugMode;
+	[SerializeField]
+	private KeyCode spawnKey;
+
+    private readonly List<ObjectPool> _priorityList = new List<ObjectPool>();
+
+	private Coroutine spawnRoutine = null;
+
+	public bool IsSpawning { get; private set; }
+
+	private void Awake()
+	{
+		BuildPriorityList();
+	}
+
+	private void Update()
+	{
+		if (debugMode)
+		{
+			if (Input.GetKeyDown(spawnKey))
+			{
+				StartCoroutine(StartSpawnSequence());
+			}
+		}
+	}
+
+	private void BuildPriorityList()
+	{
+		// Build a list that represent each pool based on its priority
+		foreach (var pool in spawnPools)
+		{
+			for (int i = 0; i < pool.Priority; i++)
+			{
+				_priorityList.Add(pool.Pool);
+			}
+		}
+	}
+
+	private IObjectPoolable GetRandomObject()
+	{
+		var pool = GetRandomPool();
+		return pool.GetAvailableObject();
+	}
+
+	private ObjectPool GetRandomPool()
+	{
+		// List entries already represent priority, just pick a random one
+		var index = Random.Range(0, _priorityList.Count);
+
+		return _priorityList[index];
+	}
+
+	private IEnumerator SpawnSequence()
+	{
+		IsSpawning = true;
+
+		for (int i = 0; i < spawnCount; i++)
+		{
+			var spawned = GetRandomObject();
+			var spawnedTransform = spawned.GetGameObject().transform;
+			var spawnedEnforcable = spawned.GetGameObject().GetComponent<IPhysicsEnforcable>();
+
+			// Prepare position and rotation
+			spawnedTransform.position = transform.position;
+			spawnedTransform.forward = Random.onUnitSphere;
+
+			// Activate physics and renders
+			spawned.Activate();
+
+			// Do start physics stuff
+			var direction = transform.forward + Random.onUnitSphere * directionFactor;
+			var force = Random.Range(spawnForce.x, spawnForce.y);
+			spawnedEnforcable.EnforceForce(direction, force);
+
+			yield return new WaitForSeconds(spawnDelay);
+		}
+
+		IsSpawning = false;
+	}
+
+	public IEnumerator StartSpawnSequence()
+	{
+		// Do not start if already spawning
+		if (IsSpawning) yield break;
+
+		spawnRoutine = StartCoroutine(SpawnSequence());
+		yield return spawnRoutine;
+	}
+}
---
+++ Assets/Scripts/Object_Spawning/SpawnPoolEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class SpawnPoolEntry
+{
+	[SerializeField]
+	private ObjectPool _pool;
+
+	[SerializeField]
+	private ushort _priority;
+
+	public ObjectPool Pool
+	{
+		get { return _pool; }
+	}
+
+	public ushort Priority
+	{
+		get { return _priority; }
+	}
+}
