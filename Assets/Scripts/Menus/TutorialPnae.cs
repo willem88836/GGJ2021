2b89d1ca184effd2ab21b0400e806700f4a3dd68
Willem Meijer <willem1998m@hotmail.com>
2021-01-31 14:24:23 +0100

    tutorial pane

---
+++ Assets/Scripts/Menus/TutorialPnae.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class TutorialPnae : MonoBehaviour
+{
+	[SerializeField] private LevelManager levelManager;
+
+	public void Start()
+	{
+		Time.timeScale = 0;
+	}
+
+	public void Stop()
+	{
+		Time.timeScale = 1;
+		gameObject.SetActive(false);
+		levelManager.BeginGame();
+	}
+
+	public void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			Stop();
+		}
+	}
+}
