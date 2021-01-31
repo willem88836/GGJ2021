f9f3fc20f5807aca460d74116267e1ab02e577fe
Willem Meijer <willem1998m@hotmail.com>
2021-01-31 12:05:47 +0100

    Persistent Tunes in menu

---
+++ Assets/Scripts/Menus/MenuAudio.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MenuAudio : MonoBehaviour
+{
+    [SerializeField] private bool enableMenuAudio;
+    private static MenuAudio singleton;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        if (enableMenuAudio)
+        {
+            if (singleton != null)
+            {
+                Destroy(this);
+            }
+            else
+            {
+                singleton = this;
+                DontDestroyOnLoad(this);
+                this.GetComponent<AudioSource>().Play();
+            }
+        }
+        else
+        {
+            if (singleton != null)
+            {
+                singleton.GetComponent<AudioSource>().Stop();
+                Destroy(singleton);
+                singleton = null;
+            }
+            Destroy(this); 
+        }
+    }
+}
